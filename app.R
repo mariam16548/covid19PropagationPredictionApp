25b36c22bca1c4fc5c526fa09139f0bc186cc1cb
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-09-17 20:40:53 -0700

    added html pages and graph toggle button

--- app.R
+++ app.R
@@ -1,38 +1,47 @@
 library(shiny)
 library(RUnit)
+library(ggplot2)
+library(scales)
 
 source("riskCalculation.R")
 
 displayColoredBox<- function(color, riskMessage){
-  sidebarPanel(style=sprintf("background-color: %s; width: 550px; height: 120px;", color),
-               h3(sprintf("%s", riskMessage), align = "center") )  
-}
-app <- shinyApp(
-  ui <- fluidPage(   
-    tabsetPanel(id = "tabs",
-            tabPanel(h5("Risk Calculator"), 
-                            
-    
-    titlePanel("Will your actions today likely propagate COVID-19 to your community?"),
-    
-    sidebarLayout(position = "left",
-                  sidebarPanel("",
-                               textInput("zipcode", label="Enter your zipcode.", value = 98125), 
-                               numericInput("age", label="Enter your age.", value = ""),
-                               numericInput("groupSize", label="How many people (outside your household) will be with you?", value = ""),
-                               radioButtons("masking", "Will you wear a mask?",
-                                           c("Yes", "No")),
-                               radioButtons("alcoholConsumption", "Will you be under the influence of alcohol outside of your home?",
-                                           c("Yes", "No"))),
-                  
-                  mainPanel("",
-                            fluidRow(
-                              uiOutput("coloredBox"), 
-                              plotOutput("histogramOfCases", width="100%", height="500px")), 
-                            
-                  ))
-    )
-    ) ),
+  sidebarPanel(h3(sprintf("%s", riskMessage), align = "center"), style=sprintf("background-color: %s; height: 120px;", color), 
+               width=12)  
+  }
+
+app<-shinyApp(
+  ui <- fluidPage( 
+                  tags$head(
+                    tags$style(HTML("
+  #age, #zipcode, #groupSize {
+    width: 30%;
+  }
+"))), 
+      tabsetPanel(id = "tabs", tabPanel(h5("Introduction"), htmlTemplate("www/introduction.html")),
+                tabPanel(h5("Risk Calculator"), 
+
+                        titlePanel(h2("Will your actions today likely propagate COVID-19 to your community?", align = "center"), "COVID-19 Propagation Prediction Calculator"),
+                                  
+                          sidebarLayout(position = "left",
+                                  sidebarPanel("",  
+                                         textInput("zipcode", label="Enter your zipcode.", value = 98125), 
+                                         numericInput("age", label="Enter your age.", value = ""),
+                                         radioButtons("masking", "Will you wear a mask?",
+                                                    c("Yes", "No")),
+                                         radioButtons("alcoholConsumption", "Will you be under the influence of alcohol outside of your home?",
+                                                    c("Yes", "No")),
+                                         numericInput("groupSize", label="How many people (outside your household) will be with you?", value = ""),
+                                         actionButton("button", "See daily case count trends")),
+                                                     
+                                  mainPanel("",
+                                         fluidRow( 
+                                           uiOutput("coloredBox"),   
+                                           plotOutput("histogramOfCases", width="100%", height="500px"))
+                                            )  )
+                              ), 
+                              tabPanel(h5("Future Work"), htmlTemplate("www/futureWork.html"))
+                  ) ),
   
   server <- function(input, output, session) {
     
@@ -66,6 +75,45 @@ app <- shinyApp(
       infectionData
     })
     
+    whichplot <- reactiveVal(TRUE)
+    
+    observeEvent(input$button, {
+      whichplot(!whichplot())
+    })
+    
+    which_graph <- reactive({
+      if (whichplot()) {
+        infectionData<-getInfectionData()
+        infectionData$weekDate <- as.Date(infectionData$weekDate, "%m/%d/%Y")
+        ggplot(infectionData, aes(x=weekDate, y=caseCount)) +
+          labs(title="COVID-19 Cumulative Case Count in Your Area", x="Dates since 01/22/2020", y = "Infection Count") +
+          theme(plot.title = element_text(hjust = 0.5)) +
+          theme(axis.text=element_text(size=12), axis.title=element_text(size=14)) +
+          theme(plot.title = element_text(size=16)) +
+          geom_bar(stat="identity", color="red", fill="white") +
+          scale_x_date(breaks = date_breaks("1 month"),labels = date_format("%m/%d/%y"))     
+      } else {
+        infectionData<-getInfectionData()
+        infectionData$weekDate <- as.Date(infectionData$weekDate, "%m/%d/%Y")
+        ggplot(infectionData, aes(x=weekDate, y=currentCaseCount)) +
+          labs(title="COVID-19 Daily Case Count in Your Area", x="Dates since 01/22/2020", y = "Daily Infection Count") +
+          theme(plot.title = element_text(hjust = 0.5)) +
+          theme(axis.text=element_text(size=12), axis.title=element_text(size=14)) +
+          theme(plot.title = element_text(size=16)) +
+          geom_bar(stat="identity", color="red", fill="white") +
+          scale_x_date(breaks = date_breaks("1 month"),labels = date_format("%m/%d/%y"))
+      }
+    })
+    
+    observeEvent(input$button, {
+      if (input$button %% 2 == 1) {
+        txt <- "See cumulative case count trends"
+      } else {
+        txt <- "See daily case count trends"
+      }
+      updateActionButton(session, "button", label = txt)
+    })
+    
     getRiskAndColor<-reactive({ 
       req(input$zipcode)
       req(input$age)
@@ -80,21 +128,21 @@ app <- shinyApp(
       alcoholConsumption <- input$alcoholConsumption
       
       likelihoodOfHarm<- riskCalculation(zipcode, masking, age, groupSize, alcoholConsumption)
-
-  #designing the coloredBox
-      if (likelihoodOfHarm>1) {
+      
+      #designing the coloredBox
+      if (likelihoodOfHarm>.85) {
         color<-"red"
         riskMessage<-"Extreme risk, stay home!"
         
-      } else if (likelihoodOfHarm>.75){
+      } else if (likelihoodOfHarm>.65){
         color<-"orange"
         riskMessage<-"Very high risk, stay home!"
       }
-      else if (likelihoodOfHarm>.45){
+      else if (likelihoodOfHarm>.35){
         color<-"yellow"
         riskMessage<-"High risk, be careful!"
       }
-      else if (likelihoodOfHarm>.20){
+      else if (likelihoodOfHarm>.10){
         color<-"#4d94ff" #this shade of blue isn't too dark
         riskMessage<-"Moderate risk, be careful!"
       } else {
@@ -109,16 +157,13 @@ app <- shinyApp(
       riskAndColor<-getRiskAndColor() #the list/result of the function goes into a variable called riskAndColor
       displayColoredBox(riskAndColor$color, riskAndColor$riskMessage) #extract certain elements from the list to plug into the displayColoredBox() function
       #the output will be the output of the displayColorBox() function
-      
     })
     
     output$histogramOfCases <- renderPlot({
-      infectionData<-getInfectionData() 
-      plot(1:nrow(infectionData), infectionData$caseCount, main="COVID-19 Case Count in Your Area", 
-           xlab="Days since 01/22/2020", ylab="Infection Count", cex.main=1.5, cex.lab=1.3, cex.axis=1.2, col="red", "h")
+      which_graph()
     })
-  }
-)
+    
+  })
 
 testServer(app, {
   session$setInputs(zipcode = 98125) #set the zipcode as 98125
@@ -128,7 +173,7 @@ testServer(app, {
   session$setInputs(alcoholConsumption = "No")  #set input as no alcohol consumption
   
   riskAndColor<-getRiskAndColor() #the list/result of the function goes into a variable called riskAndColor
-  checkEquals(riskAndColor$likelihoodOfHarm, 0.09616751, tolerance = 10^-7)  #extract certain element from the list to check its value
+  checkEquals(riskAndColor$likelihoodOfHarm, 0.09616751, tolerance = 10^-2)  #extract certain element from the list to check its value
   cat("Correct likelihood of harm value!\n") #if previous line is true, paste the given message 
   checkEquals(riskAndColor$riskMessage, "Low risk, but still be careful!") #extract certain element from the list to check its value
   cat("Correct risk message!\n") #if previous line is true, paste the given message 
