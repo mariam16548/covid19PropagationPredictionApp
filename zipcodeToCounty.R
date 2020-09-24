1068dd1326152341b6254e0889cdd479e924adf1
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-09-23 19:49:43 -0700

    Update zipcodeToCounty.R

--- zipcodeToCounty.R
+++ zipcodeToCounty.R
@@ -1,16 +1,12 @@
-library(httr) 
-urlCounties <- "https://raw.githubusercontent.com/scpike/us-state-county-zip/master/geo-data.csv"
-zipcodeToCountyData <- content(GET(urlCounties), type = 'text/csv')
-
-zipcodeToCounty<- function(zip) {
-    tbl.county <- subset(zipcodeToCountyData, zipcode == zip) 
-    
-      countyName <- tbl.county$county
-      if (length(countyName)==0) {
-        print("The selected zipcode is invalid. Please try again.")
-        
-      }  else {
-        stateName <- tbl.county$state
-        return(list(countyName, stateName))
-      }
+zipFIPSInfo <- read.csv("data/zipToFIPS.csv")
+countyFIPSInfo <- read.csv("data/FIPStoCountyAbbrev.csv")
+zipcodeToCounty <- function(zipcode) {
+  tbl.county <- subset(zipFIPSInfo, ZIP == zipcode)
+  countyFIPS <- tbl.county$COUNTY
+  tbl.countyFIPS <- subset(countyFIPSInfo, FIPS == countyFIPS)
+  countyName <- as.character(tbl.countyFIPS$Name)
+  stateAbbreviation <- tbl.countyFIPS$State
+  stateName <- state.name[match(stateAbbreviation, state.abb)]
+  return(list(countyName, stateName))
 }
+#source: https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/OSLU4G
