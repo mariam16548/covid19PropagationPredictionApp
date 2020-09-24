74c0a6d5e737bf8df18a2ec1e23d3875161b4e4c
mariam16548 <52047042+mariam16548@users.noreply.github.com>
2020-09-24 11:41:31 -0700

    3 and 4-digit zip codes now have leading zeros

--- zipcodeToCounty.R
+++ zipcodeToCounty.R
@@ -1,6 +1,11 @@
 zipFIPSInfo <- read.csv("data/zipToFIPS.csv")
+zipFIPSInfo$ZIP<-sapply(zipFIPSInfo$ZIP, function(x){if(nchar(x)==3){paste0(0,0,x)}else{x}})
+zipFIPSInfo$ZIP<-sapply(zipFIPSInfo$ZIP, function(x){if(nchar(x)==4){paste0(0,x)}else{x}})
+
 countyFIPSInfo <- read.csv("data/FIPStoCountyAbbrev.csv")
+
 zipcodeToCounty <- function(zipcode) {
+  zipcode<-sapply(zipcode, function(x){if(nchar(x)==3){paste0(0,0,x)}else{x}})
   tbl.county <- subset(zipFIPSInfo, ZIP == zipcode)
   countyFIPS <- tbl.county$COUNTY
   tbl.countyFIPS <- subset(countyFIPSInfo, FIPS == countyFIPS)
